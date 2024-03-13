aa46e43df0cd3a59a20132b7d72f31843b33e589
Sebastian Leal Sanchez <sebastianleal@cbit-online.com>
2024-03-12 20:26:28 -0500

    SL - docs(PropertyFileReader): add javadoc

--- src/main/java/com/periferia/utilities/PropertyFileReader.java
+++ src/main/java/com/periferia/utilities/PropertyFileReader.java
@@ -10,6 +10,11 @@ public class PropertyFileReader {
         throw new IllegalStateException("Utility class");
     }
 
+    /**
+     * Lee las propiedades desde un archivo de configuración y devuelve un objeto Properties.
+     *
+     * @return Un objeto Properties que contiene las propiedades cargadas desde el archivo de configuración.
+     */
     public static Properties readProperty() {
         Properties properties =  new Properties();
         try (FileInputStream input = new FileInputStream("./configFiles/config.properties")) {
@@ -20,6 +25,12 @@ public class PropertyFileReader {
         return properties;
     }
 
+    /**
+     * Lee las propiedades desde un archivo de configuración en la ruta especificada y devuelve un objeto Properties.
+     *
+     * @param rutaArchivoPropiedades La ruta del archivo de propiedades desde donde se cargarán las propiedades.
+     * @return Un objeto Properties que contiene las propiedades cargadas desde el archivo de configuración en la ruta especificada.
+     */
     public static Properties readProperty(String rutaArchivoPropiedades) {
         Properties properties =  new Properties();
         try (FileInputStream input = new FileInputStream(rutaArchivoPropiedades)) {
